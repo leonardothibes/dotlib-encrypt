e8173659337a9919b162d9b78b99603a39040eca
Leonardo Thibes <leonardothibes@gmail.com>
2018-06-11 09:51:11 -0300

    Implementando descripografia simétrica de strings

--- Dotlib.Encryption.Tests/SymmetricTest.cs
+++ Dotlib.Encryption.Tests/SymmetricTest.cs
@@ -6,13 +6,24 @@ namespace Dotlib.Encryption.Tests
     public class SymmetricTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Encrypt()
         {
             var plainText = "this is my plain text";
             var passPhrase = "this is my pass phrase";
 
             var encrypted = Symmetric.Encrypt(plainText, passPhrase);
             Assert.AreNotEqual(plainText, encrypted);
+        }
+
+        [TestMethod]
+        public void DecryptSuccess()
+        {
+            var encryptedText = "VVBXjy/jCQsBjv8hlyl0lrhcRuyaWzorLcLp86Zkqx8=";
+            var plainText = "this is my plain text";
+            var passPhrase = "this is my pass phrase";
+
+            var decrypted = Symmetric.Decrypt(encryptedText, passPhrase);
+            Assert.AreEqual(plainText, decrypted);
         }
     }
 }
